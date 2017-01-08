a0bc3113b1c5c1643dfeb6e81a44d779ee19a0a6
Vincent QB <vincentqb@gmail.com>
2017-01-08 15:53:12 -0500

    initial code example.

---
+++ kernel.py
@@ -0,0 +1,39 @@
+import kagglegym
+# import numpy as np
+# import pandas as pd
+
+
+def make_prediction(observation):
+    """Make prediction."""
+    target = observation.target
+    return target
+
+
+# Interface for code competition
+env = kagglegym.make()
+
+# Get initial observation
+observation = env.reset()
+
+# Load train dataset
+train = observation.train
+print("Train has {} rows".format(len(train)))
+
+# Template target to predict
+target = observation.target
+print("Target columns: {}".format(target.columns))
+
+while True:
+    # Check timestamp
+    timestamp = observation.features["timestamp"][0]
+    if not timestamp % 100:
+        print("Timestamp: {}".format(timestamp))
+
+    # Make prediction
+    target = make_prediction(observation)
+
+    # Submit predicted target, and get back updated observation
+    observation, reward, done, info = env.step(target)
+    if done:
+        print("Public score: {}".format(info["public_score"]))
+        break
